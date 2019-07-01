a6a39762173f7e4421716c363aa6d2662e9646ed
Andréia <andreia.leodorio@gmail.com>
2019-07-01 11:06:31 -0300

    Testando App

--- src/main/java/edu/br/whatsapp/App.java
+++ src/main/java/edu/br/whatsapp/App.java
@@ -1,5 +1,8 @@
 package edu.br.whatsapp;
 
+import edu.br.whatsapp.modelo.Mensagem;
+import edu.br.whatsapp.modelo.Usuario;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Usuario usuario1 = new Usuario();
+        usuario1.setNome("José");
+        usuario1.setNumero("8298741563");
+        
+        Usuario usuario2 = new Usuario("Ana", "8295682587");
+        
+        Mensagem msg = new Mensagem("Oi", usuario1, usuario2);
+        System.out.println(msg.getTexto());
+        System.out.println(msg.getHorario());
+        
     }
 }
